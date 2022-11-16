47c476fcafa9e863ea87eb501c971c98ae15c914
paradox <get_ant@naver.com>
2022-11-17 06:25:16 +0900

    first upload

---
+++ SpringCapstone/src/test/java/com/yoon/SpringCapstone/SpringCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.SpringCapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
